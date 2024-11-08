1c9a1e37bab12e3f5a0fa2f7db43fd0633322907
sawhil <42714217+sawhil@users.noreply.github.com>
2024-11-08 19:13:50 +0530

    [GCAL:1]: Added FullCalendar - Dummy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      headerToolbar={{
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,timeGridDay",
+      }}
+      initialView="dayGridMonth"
+    />
+  );
 }
 
 export default App;
